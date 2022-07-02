1d6185bf68829619faa59fa6371d7ee71282e7c3
Chee Wai Wong <academicsnotes@gmail.com>
2022-07-02 10:02:10 +0800

    Commit code

--- MyPracticeProject/MyPracticeProject/arraymaxer_main.cpp
+++ MyPracticeProject/MyPracticeProject/arraymaxer_main.cpp
@@ -22,4 +22,31 @@ int main() {
 
 	std::cout << " Value of bolts[10]: " << bolts[10] << std::endl;
 
+	/**
+	*  lambda expression does not have access to the variables defined in the scope that
+	*  encloses the expression
+	*  [] are called captured block.
+	*/
+
+	auto aarhus = []() {std::cout << "Solidum petit in profundis!" << std::endl; };
+	aarhus();
+	auto _aarhus = [] {std::cout << "Solidum petit in profundis!" << std::endl; };
+	_aarhus();
+
+
+	auto multiply = [](int x, int y) {std::cout << "Total: " << x * y << std::endl; };
+	multiply(7, 17);
+
+	auto _multiply = [](int x, int y) -> int {return x * y; };
+	int sum = _multiply(7, 17);
+	std::cout << "Total: " << sum << std::endl;
+
+	// This code puts the capture block to use.
+	int x = 7;
+	auto __multiply = [x](int y) -> int { return x * y; };
+	int _sum = __multiply(17);
+	std::cout << "Total: " << _sum << std::endl;
+
+
+
 }
